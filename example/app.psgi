a7f97ee7e7bd7ef8d723de1db188234b0a3068d3
mr.Rico <mrrico@mrrico-laptop>
2011-09-29 18:03:16 +0400

    fish

--- example/app.psgi
+++ example/app.psgi
@@ -2,10 +2,15 @@
 use strict;
 use warnings;
 
-use lib '../lib';
+use File::Spec;
+use Cwd 'abs_path';
+use lib File::Spec->catdir(sub{local @_=File::Spec->splitdir(File::Spec->catdir(sub{local @_ = File::Spec->splitpath(abs_path(__FILE__));@_[0..$#_-1]}->()));@_[0..$#_-1]}->(),'lib');
+
 use Plack::Builder;
 use Plack::App::DRMVC;
 
 builder {
-      Plack::App::DRMVC->new(conf_path => 'conf.ini');
+      Plack::App::DRMVC->new(
+            conf_path => File::Spec->catfile(sub{local @_ = File::Spec->splitpath(abs_path(__FILE__)); $_[$#_] = 'conf.ini'; @_}->())
+      );
 };
--- lib/Plack/App/DRMVC.pm
+++ lib/Plack/App/DRMVC.pm
@@ -6,6 +6,7 @@ our $VERSION = '0.01';
 
 use parent qw(Plack::Component);
 # use Plack::Util::Accessor qw(conf_path);
+use overload '&{}' => sub { shift->to_app(@_) }, fallback => 1;
 
 use Carp;
 use Config::Tiny;
@@ -14,14 +15,19 @@ my $s = undef;
 sub instance {$s}
 
 sub new {
+    $DB::signal = 1;
     my $class = shift;
     my $param = ref $_[0] eq 'HASH' ? $_[0] : {@_};
+    
     my $cnf = Config::Tiny->read(delete $param->{conf_path});
     croak "'conf_path'can't loaded" unless $cnf; 
-        
+    
+    #my $self = $class->SUPER::new();
+    #$self->{cnf} = 
     my $self = bless {cnf => $cnf}, $class;
     
     $self = $s;
+    return $self;
 }
 
 sub cnf {$_[0]->{cnf}}
