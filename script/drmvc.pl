82c5da4353194c078b7e11e3f87227108c13e8c1
mr.Rico <mrrico@mrrico-laptop>
2011-10-17 12:19:11 +0400

    script-creator has been worked succefull

--- script/drmvc.pl
+++ script/drmvc.pl
@@ -2,13 +2,65 @@
 use strict;
 use warnings;  
 
+package DRMVCreator;
+
 use File::Path qw(mkpath);
 use File::Spec;
 use Cwd;
+use Carp;
+
+my $vars = {
+    app => 'dedededede'
+};
+
+sub process_file {
+    my ($class, $content, $param, $file) = @_;
+
+    $content =~ s/{{\s*([^\s]*)?\s*}}/$param->{$1}/gme;
+    
+    open(F, ">$file") or croak $!;
+        print F $content; 
+    close(F);
+    
+    return;
+}
+
+=head3 get_data_section($name)
+
+Read DATA section from main package and will be returned sub-section $name.
+There's something like Data::Section::Simple 
+
+=cut
+my $data = undef;
+sub get_data_section {    
+    my $name = shift;
+    return unless $name;
+    unless ($data) {
+        my ($key, $val) = ('','');
+        while (<main::DATA>) {
+            if (/\s*\@{2}\s*(.*)?\s*/) {
+                if ($key) {
+                   chomp $val;
+                   $data->{$key} = $val;
+                   $val = ''; 
+                }
+                $key = $1;
+            } else {
+                $val .= $_;
+            }
+        }
+        chomp $val;
+        $data->{$key} = $val if $key;
+    }
+    return $data->{$name};
+}
 
 sub create_folder_structure {
     my $class = shift;
     my $app = shift;
+    
+    $vars->{app} = $app; 
+    
     my $structure = $class->get_folder_structure($app);
     
     $class->_create_folder_structure($structure);
@@ -21,7 +73,17 @@ sub _create_folder_structure {
     my $dir_hash = shift;
     
     for (keys %$dir_hash) {
-        
+        unless (defined $dir_hash->{$_}) {
+            # skip
+            1;
+        } elsif (ref $dir_hash->{$_} eq 'HASH') {
+            mkpath($_, {verbose => 1});
+            $class->_create_folder_structure($dir_hash->{$_});            
+        } elsif (not ref $dir_hash->{$_}) {
+            $class->process_file($dir_hash->{$_}, $vars, $_);
+        } else {
+            croak "something wrong";
+        }
     }
     
 }
@@ -33,53 +95,64 @@ sub get_folder_structure {
     my $app = lc $APP;
     $app =~ s/::/_/g;
     my $c_path = cwd;
-    my $st = {
-        File::Spec->catdir($c_path, 'lib') => {
-            
-        },
-        File::Spec->catdir($c_path, 'log') => undef,
-        File::Spec->catdir($c_path, 'static') => {
-            File::Spec->catdir($c_path, 'static', 'js')  => undef,
-            File::Spec->catdir($c_path, 'static', 'img') => undef 
-        },
-        File::Spec->catdir($c_path, 'conf') => {
-            File::Spec->catdir($c_path, 'conf', 'access') => {
-                File::Spec->catfile($c_path, 'conf', 'access', 'AllowTo.mini') => 'AllowTo.mini',
-                File::Spec->catfile($c_path, 'conf', 'access', 'DenyTo.mini')  => 'DenyTo.mini',
-            },
-            File::Spec->catfile($c_path, 'conf', 'conf.mini') => 'conf.mini'
-        },
-        File::Spec->catfile($c_path, "${app}.psgi") => 'app.psgi' 
-    };
     
-    my $lst = $st->{lib};
-    my $last;
-    for $last (split('::', $APP)) {
-        $lst->{$_} = {};
-        $lst = $lst->{$_};
+    my $st = {}; 
+    my $c_dir = File::Spec->catdir($c_path);
+    my $lst = $st;
+    my @app = split('::', $APP);
+    for (@app) {
+        $c_dir = File::Spec->catdir($c_dir, $_);
+        $lst->{$c_dir} = {};
+        $lst = $lst->{$c_dir};
     };
     
     %$lst = (
-        'Controller' => {
-            'Root.pm' => 'Root.pm' 
+        File::Spec->catdir($c_dir, 'lib') => {},
+        File::Spec->catdir($c_dir, 'log') => undef,
+        File::Spec->catdir($c_dir, 'static') => {
+            File::Spec->catdir($c_dir, 'static', 'js')  => undef,
+            File::Spec->catdir($c_dir, 'static', 'img') => undef 
         },
-        'Model' => undef,
-        'View'  => undef,
-        'Exception' => undef,
-        'Extend' => {
-            'Controller' => {
-                'Attributes' => undef
+        File::Spec->catdir($c_dir, 'conf') => {
+            File::Spec->catdir($c_dir, 'conf', 'access') => {
+                File::Spec->catfile($c_dir, 'conf', 'access', 'AllowTo.mini') => get_data_section('AllowTo.mini'),
+                File::Spec->catfile($c_dir, 'conf', 'access', 'DenyTo.mini')  => get_data_section('DenyTo.mini'),
             },
-            'Request.pm' => 'Request.pm',
-            'Response.pm' => 'Response.pm',
-            'Dispatcher.pm' => 'Dispatcher.pm'
+            File::Spec->catfile($c_dir, 'conf', 'conf.mini') => get_data_section('conf.mini')
         },
-        "${last}.pm" => 'DRMVC.child'
+        File::Spec->catfile($c_dir, "${app}.psgi") => get_data_section('app.psgi') 
+    );
+    
+    $c_dir = File::Spec->catdir($c_dir, 'lib');
+    $lst = $lst->{$c_dir};
+    for (@app) {
+        $c_dir = File::Spec->catdir($c_dir, $_);
+        $lst->{$c_dir} = {};
+        $lst = $lst->{$c_dir};
+    };
+    
+    %$lst = (
+        File::Spec->catdir($c_dir, 'Controller') => {
+            File::Spec->catfile($c_dir, 'Controller', 'Root.pm') => get_data_section('Root.pm') 
+        },
+        File::Spec->catdir($c_dir, 'Model') => undef,
+        File::Spec->catdir($c_dir, 'View')  => undef,
+        File::Spec->catdir($c_dir, 'Exception') => undef,
+        File::Spec->catdir($c_dir, 'Extend') => {
+            File::Spec->catdir($c_dir, 'Extend','Controller') => {
+                File::Spec->catdir($c_dir, 'Extend','Controller','Attributes') => undef
+            },
+            File::Spec->catfile($c_dir, 'Extend','Request.pm') => get_data_section('Request.pm'),
+            File::Spec->catfile($c_dir, 'Extend','Response.pm') => get_data_section('Response.pm'),
+            File::Spec->catfile($c_dir, 'Extend','Dispatcher.pm') => get_data_section('Dispatcher.pm')
+        },
+        File::Spec->catfile($c_dir, $app[-1].".pm") => get_data_section('DRMVC.child')
     );
     
     return $st;
 }
 
+1;
 
 package Helper;
 
@@ -87,173 +160,60 @@ sub help {
     my ($class, $reson) = @_;
     print "    Error: ",$reson,"\n\n" if $reson;
     print <<END;
-    Create your xxxxx application:
-            perl drmvc.pl --app Foo::Bar
+    Create your application:
+            perl /some/path/drmvc.pl --app Foo::Bar
 END
 exit;
 }
 
 1;
-#-----------------------------------------------------------------
-package SimpleStash;
 
-my $s_param = {};
 
-sub param {
-	my $class = shift;
-	if (@_) {
-		my $key = shift;
-		if (@_) {
-			$s_param->{$key} = $_[0];
-			1;
-		} else {
-			$s_param->{$key};
-		}
-	} else {
-		$s_param;
-	}
-}
-
-1;
-# ----------------------------------------------------------------
-package Creator;
-
-require Bicycle;
-use File::Spec;
-use File::Path qw(make_path);
-use Module::Util qw(find_installed);
-use Template;
-
-# путь к папке с шаблонами для Template в absolute
-my $tmp_path = File::Spec->catdir([File::Spec->splitpath( find_installed('Bicycle') )]->[1],'Bicycle','tmpl');
-my $tt = Template->new(
-    INCLUDE_PATH => $tmp_path,
-    ABSOLUTE => 0,
-    RELATIVE => 1
-);
-
-sub create_file {
-    my ($class, $dir, $file, $new_name) = @_;
-    
-    my $target = File::Spec->catfile($dir, $file);
-    if (-e $target) {
-        # файл найден, перезаписывать не будем
-        warn("file $target already exists");
-    } elsif (-e File::Spec->catfile($tmp_path, $file)) {
-        # если файла нет, а шаблон есть, то рендерим
-        $target = File::Spec->catfile($dir, $new_name) if $new_name;
-        my $META = {};
-        $tt->process($file, { META => $META, %{SimpleStash->param} }, $target) || die $tt->error;
-        if ($META->{chmod}) {
-            chmod $META->{chmod}, $target;
-        }
-    } else {
-        # если шаблона нет, просто создаём
-        open(FAKE,">".$target) or die $!;
-        close(FAKE);
-    }
-    
-    return;    
-}
-
-sub create_dir {
-    my ($class, @dir) = @_;
-    
-    my $dir = File::Spec->catdir(@dir); 
-    #make_path($dir,{mode => 0664});
-    make_path($dir);
-    
-    return $dir;
-}
-
-1;
-# ----------------------------------------------------------------
 package main;
-
 use Getopt::Long;
-use Cwd;
-use File::Spec;
-use Date::Manip;
 
-my ($help,$app_name) = ();
+my ($help, $app_name) = ();
 
 GetOptions(
     'help|h=i'      => \$help,
-    'app=s'       => \$app_name
+    'app=s'         => \$app_name
 );
 
 # check param
 Helper->help() if $help;
-Helper->help("app name '".$app_name."' not valid") if (not $app_name or $app_name !~ /^\w+$/);
-$app_name = ucfirst $app_name;
+Helper->help("app name '".$app_name."' not valid") if (not $app_name or $app_name !~ /^\w+(:{2}\w+)+$/);
+$app_name =~ s/^\s+//;
+$app_name =~ s/\s+$//;
 
-my $main_dir = Creator->create_dir(cwd,$app_name);
-
-my $app_folder = {
-    lib => {
-        $app_name => {
-            Model => undef,
-            Controller => ['Root.pm'],
-            # !!!TextXslate
-            View => ['TextXslate.pm', 'TT.pm'],
-            Exception => ['500.pm','404.pm','400.pm', '403.pm'],         
-            Extend => {
-                single_file => ['Request.pm','Response.pm', 'Dispatcher.pm']
-            }
-        },
-        my_app_file => [$app_name.'.pm']
-    },
-    script => ['main.psgi', 'creator.pl','session_cleanup.sh'],
-    log => [],
-    files => {
-        static => undef,
-        webcached => undef
-    },
-    conf => ['main.xml'],
-    data => {
-        session => undef,
-        tmp     => undef
-    },
-    tmpl => ['test.tx']
-};
-
-# переменные для шаблона
-SimpleStash->param('CATFILE', sub{File::Spec->catfile(@_)});
-SimpleStash->param('CATDIR',  sub{File::Spec->catdir(@_)});
-
-SimpleStash->param('app_name', $app_name);
-SimpleStash->param('app_dir', $main_dir);
-SimpleStash->param('timezone', Date::Manip::Date_TimeZone());
-SimpleStash->param('lbr', '[');
-SimpleStash->param('rbr', ']');
-
-create($app_folder, $main_dir);
-
-# рекурсивно создаём каталог с файлами
-sub create {
-   my ($el, $folder) = @_;
-   
-   # skip - empty folder
-   return unless $el;
-   
-   if (ref $el eq 'ARRAY') {
-       Creator->create_file($folder, $_) for @$el;
-   } elsif (ref $el eq 'HASH') {
-       for (keys %$el) {
-           if ($_ eq 'single_file') {
-               if (ref $el->{$_} eq 'ARRAY') {
-                Creator->create_file($folder, $_) for @{$el->{$_}};
-               }
-           } elsif ($_ eq 'my_app_file') {
-               Creator->create_file($folder, 'MyApp.pm' => $app_name.'.pm');
-           } else {
-               my $dir = Creator->create_dir($folder, $_);
-               create($el->{$_}, $dir);
-           }
-       }
-   }
-   
-   return;
-}
+DRMVCreator->create_folder_structure($app_name);
 
 exit;
+ __DATA__
+@@ conf.mini
+conf.mini
+{{ app }}
+
+@@ AllowTo.mini
+AllowTo.mini
+
+@@ DenyTo.mini
+DenyTo.mini
+
+@@ app.psgi
+app.psgi
+
+@@ DRMVC.child
+DRMVC.child
+
+@@ Root.pm
+Root.pm
+
+@@ Request.pm
+Request.pm
+
+@@ Response.pm
+Response.pm
+
+@@ Dispatcher.pm
+Dispatcher.pm
+
