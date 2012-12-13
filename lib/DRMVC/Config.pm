80d54d2b7206ad80e0bc2215361feef1ecf8024e
mr.Rico <catamoose@yandex.ru>
2012-12-13 11:33:31 +0400

    - version with config

--- lib/DRMVC/Config.pm
+++ lib/DRMVC/Config.pm
@@ -4,13 +4,14 @@ use warnings;
 
 use Carp;
 use File::Spec;
+use Fcntl;
 use base qw(Config::Mini);
 
 my $instance = undef;
 sub instance { $instance }
 
 sub section {
-    my $self = shift;
+    my $self = shift->instance();
     # Config::Mini return undefine value on unexists key in config
     my @ret = $self->SUPER::section(@_);
     if (@_ and not defined $ret[0]) {
@@ -23,7 +24,8 @@ sub section {
 }
 
 sub add {
-    my ($self, $section, $key, $val) = @_;
+    my $self = shift->instance();
+    my ($section, $key, $val) = @_;
     my $s = $self->section($section);
     $s->{$key} = $val;
     unshift @{$s->{'__'.$key}}, $val; 
@@ -31,18 +33,20 @@ sub add {
 }
 
 sub get {
-    my ($self, $section, $key, $default) = @_;
+    my $self = shift->instance();
+    my ($section, $key, $default) = @_;
     my $ret = $self->_get_set($section, $key);
     return (defined $default and not defined $ret) ? $default : $ret; 
 }
 
 sub getset {
     my $self = shift;
-    return $self->_get_set(@_);
+    return $self->instance()->_get_set(@_);
 }
 
 sub _get_set {
-    my ($self, $section, $key, $default) = @_;
+    my $self = shift->instance();
+    my ($section, $key, $default) = @_;
     my $s = $self->section($section);
     my $ret = $s->{$key};
     if (not defined $ret and defined $default) {
@@ -55,30 +59,18 @@ sub _get_set {
 sub new {
     my $class = shift;
     my $cnf_file = shift || '';
+    unless ($cnf_file) {
+        my @private = getpwuid($<);
+        my $dir = File::Spec->catfile($private[7], '.drmvc');
+        mkdir($dir) unless -d $dir;
+        $cnf_file = File::Spec->catfile($dir, 'config');
+        if ( sysopen(my $dummy, $cnf_file, O_RDWR|O_NONBLOCK|O_CREAT) ) {
+            $dummy->close;
+        }
+    }
     $instance = $class->SUPER::new($cnf_file);
-    unless ($instance) {
-        carp "file '$cnf_file' can't loaded";
-        return;
-    };
-    
-#    # important check
-#    my $path = $instance->section('general')->{'path'};
-#    my $data_path   = $instance->getset('data', 'dir', File::Spec->catdir($path, 'data'));
-#    my $log_path    = $instance->getset('logger', 'dir', File::Spec->catdir($path, 'logger'));
-#    for ($data_path, $log_path) {
-#        unless (-e $_ and -d _) {
-#            unless (mkdir $_) {
-#                carp "$_ has error: $!";
-#                return;
-#            }
-#        };
-#    }
-    
-    return 1;
 }
 
 
 1;
 __END__
-
-# MANIFEST
